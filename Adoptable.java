74b30cfa7ddeb73b7b8ed4fc84729b71bf572934
An Vo <134673103+annievo1611@users.noreply.github.com>
2024-12-15 22:56:04 -0600

    Adoptable.java

--- Adoptable.java
+++ Adoptable.java
@@ -1,7 +1,6 @@
 /**
-* Name: An Vo
 * Date: 12-11
-* Description: Adoptable interface includes one method that determine if an object has been adopted.
+* Description: Adoptable interface includes one method that determines if an object has been adopted.
 */
 
 public interface Adoptable {
